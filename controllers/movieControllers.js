543b23a224e87aa1155c3d5d05a6a283cfb68f33
GiuntaGiovanni <jhonniest741@gmail.com>
2025-03-10 16:43:13 +0100

    [feat] add in show funtion single detail movie request e response

--- controllers/movieControllers.js
+++ controllers/movieControllers.js
@@ -15,7 +15,39 @@ function index(req, res) {
     })
 }
 // show
-function show(req, res) { { res.json('questa è la rotta show') } }
+function show(req, res) {
+    // salviamo l'id dalla richiesta
+    const { id } = req.params
+    // salviamo la query sql del singolo movie
+    const movieSql = `
+                    SELECT*
+                    FROM movies
+                    WHERE id = ?;
+                `;
+    // salviamo la query sql delle reviews
+    const reviewsSql = `
+                SELECT*
+                FROM reviews
+                WHERE movie_id = ?;
+                `;
+
+    // eseguiamo la query
+    connection.query(movieSql, [id], (err, movieResults) => {
+        if (err) return res.status(500).json({ error: 'Database query failed' })
+        if (movieResults.length === 0) return res.status(404).json({ error: 'film non trovato' });
+        // recuperiamo il film
+        const movie = movieResults[0];
+
+        connection.query(reviewsSql, [id], (err, reviewsResults) => {
+            if (err) return res.status(500).json({ error: 'Database query failed' })
+            if (reviewsResults.length === 0) return res.status(404).json({ error: 'non ci sono reviews' });
+            // inseriamo le reviews nel film
+            movie.reviews = reviewsResults;
+            res.json(movie);
+        })
+    })
+
+}
 // posts
 function store(req, res) { res.json('questa è la rotta store') }
 //update
