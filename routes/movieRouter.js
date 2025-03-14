53d17290a571b45a57a46eddfce98c5b58906db3
GiuntaGiovanni <jhonniest741@gmail.com>
2025-03-14 15:24:42 +0100

    [feat] import multer in router & add mediafiles in post route

--- routes/movieRouter.js
+++ routes/movieRouter.js
@@ -3,10 +3,13 @@ const express = require('express');
 // importiamo express router
 const router = express.Router();
 // importiamo i controllers
-const controllers = require('../controllers/movieControllers')
+const controllers = require('../controllers/movieControllers');
+// importiamo il multer
+const mediaFiles = require('../middlewares/multer')
+
 
 // destrutturiamo i controllers
-const { index, show, store, update, modify, destroy } = controllers
+const { index, show, store, storeReview, update, modify, destroy } = controllers
 
 // Rotte CRUD per la gestione dei dati del db:movies
 // index
@@ -14,9 +17,9 @@ router.get('/', index);
 // show
 router.get('/:id', show);
 // store
-router.post('/', store);
+router.post('/', mediaFiles, store);
 // store review
-router.post('/:id/reviews', store);
+router.post('/:id/reviews', storeReview);
 // update
 router.put('/:id', update)
 // modify
